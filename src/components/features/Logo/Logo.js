b9a2d799804563185d2cf5093d38ca8751b43a11
Anna Szmyga <anna.szmyga@o2.pl>
2019-07-31 16:01:17 +0200

    Add logo

--- src/components/features/Logo/Logo.js
+++ src/components/features/Logo/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Logo.css';
 
 const Logo = (props) => {
-        return <div>LOGO SKLEPU</div>
+        return <div className="logo">WonderCake</div>
 }
 
 export default Logo;
