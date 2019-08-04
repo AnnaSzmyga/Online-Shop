84aaeee5877e3705f88cc14c5ab47e1f006c648d
Anna Szmyga <anna.szmyga@o2.pl>
2019-08-04 21:07:13 +0200

    Fix addProduct action

--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -10,9 +10,24 @@ export default function cartReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_PRODUCT:
         console.log("dodawanie do koszyka");
-        const cartProduct = action.product;
-        cartProduct.quantity = 1;
-        return [cartProduct, ...state];
+
+        let doubbleProduct = false;
+
+        // check if product is already in the cart
+        state.map((product) => {
+            if (product.id === action.product.id) {
+                product.quantity += 1;
+                doubbleProduct = true;
+            }
+            return product;
+        });
+
+        //add new product to cart
+        if (!doubbleProduct) {
+            const cartProduct = action.product;
+            cartProduct.quantity = 1;
+            return [cartProduct, ...state];
+        } else return state;
 
     case REMOVE_PRODUCT:
         console.log("usuwanie z koszyka");
