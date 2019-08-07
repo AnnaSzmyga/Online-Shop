48ec145b117d9b0b36525761b09cc7c96666db5b
Anna Szmyga <anna.szmyga@o2.pl>
2019-08-07 14:38:54 +0200

    Extend and style NoMatch component

--- src/components/pages/NoMatch/NoMatch.js
+++ src/components/pages/NoMatch/NoMatch.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Fade } from 'reactstrap';
 
+import './NoMatch.css';
+
 const NoMatch = (props) => {
     return (
-        <Fade timeout={100}>
-            <div>Strona nie została znaleziona.</div>
-            <Link to="/">Powrót na stronę główną</Link>
+        <Fade className="no-match" timeout={100}>
+            <div className="no-match__error">404</div>
+            <div className="no-match__info">Strona nie została znaleziona.</div>
+            <Link className="no-match__link" to="/">
+                <div className="go-back" >
+                    <i className="fas fa-arrow-left go-back__icon"></i>
+                    Powrót na stronę główną
+                </div>
+            </Link>
         </Fade>
     )
 }
